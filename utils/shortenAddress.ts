10d9bc0021d65a177cf983fd2ee2132ead989347
yeungparkhay <yeungparkhay@gmail.com>
2022-10-02 01:09:49 +0100

    Updated shortenAddress to allow custom length

--- utils/shortenAddress.ts
+++ utils/shortenAddress.ts
@@ -1,3 +1,3 @@
-export const shortenAddress = (address: string): string => {
-    return `${address.slice(0, 5)}...${address.slice(-4)}`
+export const shortenAddress = (address: string, length?: number): string => {
+    return `${address.slice(0, length ? length : 5)}...${address.slice(-1 * (length ? length - 1 : 4))}`
 }
